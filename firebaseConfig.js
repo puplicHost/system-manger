b1c8b6aaf1c03e42bde5604203791f77d1536d5c
puplic Host <109622670+puplicHost@users.noreply.github.com>
2025-06-08 12:27:41 +0300

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB_iGofl6_nNdSFcjFgR2rWvxDqkPwkLG4",
+  authDomain: "business-7c5c4.firebaseapp.com",
+  projectId: "business-7c5c4",
+  storageBucket: "business-7c5c4.firebasestorage.app",
+  messagingSenderId: "48603673777",
+  appId: "1:48603673777:web:c07637ad7a25eb33bf5e90",
+  measurementId: "G-DV203G2PDN"
+};
